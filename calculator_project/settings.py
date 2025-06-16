5d31f3d109b448af3ed250efbfdc465d5cb02698
Soham Sharma <sohamnsharma@gmail.com>
2025-06-16 16:28:39 +0530

    allowed hosts

--- calculator_project/settings.py
+++ calculator_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,8 +25,11 @@ SECRET_KEY = 'django-insecure-)@4g251erb7oy-%v0kxd5b)f82xc5gk5^#2snn177b@huk6hc9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    os.getenv('RAILWAY_STATIC_URL', '').replace('https://', '').strip('/'),
+]
 
 # Application definition
 
